7b892fbe9c0f3cc6aee82da685e89e98b5aeeff0
Splatage.com <81358782+splatage@users.noreply.github.com>
2022-09-01 20:05:30 +1200

    Update debianPostInstall.sh

--- debianPostInstall.sh
+++ debianPostInstall.sh
@@ -1,1 +1,7 @@
+## Java 16
+
+curl  -O https://download.java.net/java/GA/jdk16.0.2/d4a915d82b4c4fbb9bde534da945d746/7/GPL/openjdk-16.0.2_linux-x64_bin.tar.gz
+tar -xvf openjdk-16.0.2_linux-x64_bin.tar.gz
+mv jdk-16.0.2 /opt/
+/opt/jdk-16.0.2/bin/java -version
 
