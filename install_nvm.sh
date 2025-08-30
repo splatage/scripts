4c6ef23deea5a241d3fdb4b0fad3b8f10c2f2b75
Splatage.com <81358782+splatage@users.noreply.github.com>
2025-08-30 16:13:32 +1200

    Update install_nvm.sh

--- install_nvm.sh
+++ install_nvm.sh
@@ -7,5 +7,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 source ~/.bashrc
-nvm install latest
+nvm install v22.19.0
 EOF
